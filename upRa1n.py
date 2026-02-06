7f578c2fad3069148f494b1ae12a572e43567217
ZeroxDev <zeroxxdev@gmail.com>
2026-02-06 17:13:10 +0300

    Update upRa1n.py

--- upRa1n.py
+++ upRa1n.py
@@ -319,10 +319,10 @@ def main():
         elif "restore" in sys.argv[1]:
             pass
         else:
-            print("Usage: python3 upRa1n.py <options>\n\nCommands:\n\n   restore               Tethered dualboot iOS 18 on iPad 6\n   boot                  Boot your device into iOS 18\n\nExample:\n\n   python3 upRa1n.py restore\n   python3 upRa1n.py boot\n")
+            print("Usage: sudo python3 upRa1n.py <options>\n\nCommands:\n\n   restore               Tethered dualboot iOS 18 on iPad 6\n   boot                  Boot your device into iOS 18\n\nExample:\n\n   sudo python3 upRa1n.py restore\n   sudo python3 upRa1n.py boot\n")
             sys.exit()
     except Exception as e:
-        print("Usage: python3 upRa1n.py <options>\n\nCommands:\n\n   restore               Tethered dualboot iOS 18 on iPad 6\n   boot                  Boot your device into iOS 18\n\nExample:\n\n   python3 upRa1n.py restore\n   python3 upRa1n.py boot\n")
+        print("Usage: sudo python3 upRa1n.py <options>\n\nCommands:\n\n   restore               Tethered dualboot iOS 18 on iPad 6\n   boot                  Boot your device into iOS 18\n\nExample:\n\n   sudo python3 upRa1n.py restore\n   sudo python3 upRa1n.py boot\n")
         sys.exit()
 
 
